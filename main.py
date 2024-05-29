dc9ca4d399b11574d6ec528e451b1c113af952f7
dirrgin <antihobbit@protonmail.com>
2024-05-29 21:48:36 +0300

    refactor main() and better blob reading

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
+from datetime import datetime
 from manager import \
     IoTHubRegistryManager, \
     receive_twin_reported, \
     twin_desired, json,\
-    clear_desired_twin,\
-     asyncio, os,load_dotenv,\
-    BlobServiceClient, monitor_blob_container
+    clear_desired_twin,Twin, TwinProperties,\
+    asyncio, os,load_dotenv,\
+    BlobServiceClient, get_most_recent_blob, process_blob
 
 load_dotenv()
 BLOB_CONNECTION_STRING = os.getenv("BLOB_CONNECTION_STRING")
@@ -17,13 +18,24 @@ async def main():
     registry_manager = IoTHubRegistryManager(CONNECTION_STRING_MANAGER)
     await clear_desired_twin(registry_manager, DEVICE_ID)
     blob_service_client = BlobServiceClient.from_connection_string(BLOB_CONNECTION_STRING)
-    
+    last_processed_name = None
+    last_processed_time = datetime.min  # Initialize to the earliest possible datetime
     try:
         while True:        
             twin_reported = await receive_twin_reported(registry_manager, DEVICE_ID)
-            
-            await twin_desired(registry_manager, DEVICE_ID, twin_reported)
-            await monitor_blob_container(blob_service_client, BLOB_CONTAINER_NAME, registry_manager)
+            kpi_container_name = BLOB_CONTAINER_NAME
+            container_client = blob_service_client.get_container_client(kpi_container_name)
+            try:
+            # List blobs synchronously
+                most_recent_blob = await get_most_recent_blob(container_client, last_processed_name, last_processed_time)
+                if most_recent_blob:
+                    last_processed_name, last_processed_time = await process_blob(most_recent_blob, registry_manager, twin_reported, container_client)
+                else:
+                    print("no fresh blobs. last last_processed_name is ", last_processed_name)
+            except Exception as e:
+                print(f"Exception: {str(e)}")
+            await asyncio.sleep(60)
+                        
 
     except Exception as e:
         print("Progam stoped")
--- manager.py
+++ manager.py
@@ -14,13 +14,8 @@ async def receive_twin_reported(manager_client, device_id):
 
 async def twin_desired(manager_client, device_id, reported):
     desired_twin = {}
-
-    reported.pop("$metadata", None)
-    reported.pop("$version", None)
-    
     for key, value in reported.items():
         desired_twin[key] = {"ProductionRate": value["ProductionRate"]}
-
     twin = manager_client.get_twin(device_id)
     twin_patch = Twin(properties=TwinProperties(desired=desired_twin))
     twin = manager_client.update_twin(device_id, twin_patch, twin.etag)
@@ -36,46 +31,43 @@ async def clear_desired_twin(manager, device_id):
     twin = manager.update_twin(device_id, twin_patch, twin.etag)
     print("desired prop were just cleaned")
 
-# Function to read the content of a blob asynchronously
 async def read_blob_content(blob_client):
-    stream = blob_client.download_blob()
-    blob_data = stream.readall()
-    return blob_data.decode('utf-8')
+    downloader =  blob_client.download_blob()
+    return downloader.readall().decode('utf-8')
 
-async def monitor_blob_container(blob_service_client, container_name, iot_registry_manager):
-    container_client = blob_service_client.get_container_client(container_name)
-    
-    # Keep track of the last processed blob and its length
-    last_blob_name = None
-    last_blob_length = 0
-    
-    while True:
-        # List blobs synchronously
-        blobs = container_client.list_blobs()
-        
-        for blob in blobs:
-            blob_client = container_client.get_blob_client(blob.name)
-            #blob_properties = await blob_client.get_blob_properties()
+async def get_most_recent_blob(container_client, last_processed_name, last_processed_time):
+    blobs = container_client.list_blobs()
+    # Sort blobs by last_modified attribute, most recent first
+    sorted_blobs = sorted(blobs, key=lambda blob: blob['last_modified'], reverse=True)
+    for blob in sorted_blobs:
+        if (blob.name != last_processed_name) and (blob['last_modified'].replace(tzinfo=None) > last_processed_time):
+            return blob
+    return None
 
-            # Read the full content of the blob
-            blob_content = await read_blob_content(blob_client)
-            
-            # If it's the first time processing this blob, or if it has grown, process new data
-            if blob.name != last_blob_name or len(blob_content) > last_blob_length:
-                new_data = blob_content[last_blob_length:]  # Only process new content
-                if container_name == "production-rate":
-                    json_lines = new_data.splitlines()
-                    updated_devices = {}
-                    # Process each JSON line
-                    for line in json_lines:
-                        data = json.loads(line)  # Parse the JSON line into a dictionary
-                        prod_rate = data['ProductionRate'] - 10
-                        if prod_rate < 0:
-                            prod_rate = 0
-                        updated_devices[data['DeviceName']] = {"ProductionRate": prod_rate}
-                    await twin_desired(iot_registry_manager, DEVICE_ID, updated_devices)
-               
-                last_blob_name = blob.name
-                last_blob_length = len(blob_content)
+
+async def process_blob(blob, registry_manager, twin_reported, container_client):
+    blob_client = container_client.get_blob_client(blob.name)
+    new_data = await read_blob_content(blob_client)
+
+    # Process new data
+    # new_data = blob_content 
+    twin_reported.pop("$metadata", None)
+    twin_reported.pop("$version", None)
+    json_lines = new_data.splitlines()
+
+    # Process each JSON line
+    for line in json_lines:
+        data = json.loads(line)  # Parse the JSON line into a dictionary
+        prod_rate = data['ProductionRate'] - 10
+        print(data)
+        print("its new PR: ", prod_rate)
+        formatted_key = data['DeviceName'].replace(" ", "")  # Remove spaces from the key
+        if prod_rate < 0:
+            prod_rate = 0
+        twin_reported[formatted_key] = {"ProductionRate": prod_rate}
+        twin = registry_manager.get_twin(DEVICE_ID)
+        twin_patch = Twin(properties=TwinProperties(desired=twin_reported))
+        twin = registry_manager.update_twin(DEVICE_ID, twin_patch, twin.etag)
+    return blob.name, blob['last_modified'].replace(tzinfo=None)
         
         
