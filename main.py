8fe28e2385a2b3e4aa735db621ba782de1ae84db
doublebubblemethod <antihobbit@protonmail.com>
2024-06-21 14:31:39 +0200

    service bus communication and dynamic device creation

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import sys, aioconsole
 from manager import \
     IoTHubRegistryManager, \
-    receive_twin_reported, \
-    clear_desired_twin,process_production, handle_c2d_message,\
-    asyncio, os,load_dotenv,get_most_recent_blob,\
-    BlobServiceClient, run_res_error, process_error_dm, datetime
+    receive_twin_reported,aioconsole, \
+    clear_desired_twin,process_production,\
+    asyncio, os,load_dotenv,get_most_recent_blob, receive_device_ids, \
+    BlobServiceClient, run_res_error, process_error_dm, datetime, send_device_connection_keys
+import sys
 
 load_dotenv()
 BLOB_CONNECTION_STRING = os.getenv("BLOB_CONNECTION_STRING")
@@ -12,10 +12,16 @@ CONNECTION_STRING_MANAGER = os.getenv("CONNECTION_STRING_MANAGER")
 DEVICE_ID = os.getenv("DEVICE_ID")
 KPI_CONTAINER_NAME = "production-rate"
 ERROR_CONTAINER_NAME = "device-errors-output"
+
 async def main():
- 
     registry_manager = IoTHubRegistryManager(CONNECTION_STRING_MANAGER)
-    await clear_desired_twin(registry_manager, DEVICE_ID)
+    productionLine, connection_strs= await receive_device_ids(registry_manager)
+    try:
+        await send_device_connection_keys(connection_strs)
+    except Exception as send_e:
+        print(f"error when sending a message to the 2Q")
+    for device in productionLine:
+        await clear_desired_twin(registry_manager, device)
     blob_service_client = BlobServiceClient.from_connection_string(BLOB_CONNECTION_STRING)
     kpi_last_processed_name  = None
     error_last_processed_name = None
@@ -23,20 +29,24 @@ async def main():
     error_last_processed_time = datetime.min 
     err_last_positions = {}
     kpi_last_positions = {}
+    twin_reported_dir = []
     try:
-        while True:        
-            twin_reported = await receive_twin_reported(registry_manager, DEVICE_ID)
+        while True:      
+            for device in productionLine:  
+                twin_reported = await receive_twin_reported(registry_manager, device)
+                twin_reported_dir.append(twin_reported)
             kpi_container_name = KPI_CONTAINER_NAME
             kpi_container_client = blob_service_client.get_container_client(kpi_container_name)
             error_container_name = ERROR_CONTAINER_NAME
             error_container_client = blob_service_client.get_container_client(error_container_name)
-            await run_res_error(registry_manager, 'Device 1')
+
+            for device in productionLine:  
+                await run_res_error(registry_manager, device)
             
-            '''
             try:
                 new_data1, kpi_last_processed_name, kpi_last_processed_time, kpi_last_positions = await get_most_recent_blob(kpi_container_client, kpi_last_processed_name, kpi_last_processed_time, kpi_last_positions)
                 if new_data1:
-                    await process_production(registry_manager, twin_reported, new_data1)
+                    await process_production(registry_manager, twin_reported_dir, new_data1)
                     print(f"New data from blob {kpi_last_processed_name} arrived")
                 else:
                     print("No new data found")
@@ -61,7 +71,7 @@ async def main():
                             await run_res_error(registry_manager, answer)
             except Exception as ee:
                 print(f"Exception with Error Container process: {ee}")
-            '''
+            
             await asyncio.sleep(60)
                         
 
--- manager.py
+++ manager.py
@@ -1,11 +1,82 @@
-import asyncio, logging, json, os, aiohttp
-from datetime import datetime, timedelta
+import asyncio, json, os, aiohttp
 from dotenv import load_dotenv
 from azure.iot.hub import IoTHubRegistryManager
 from azure.iot.hub.models import Twin, TwinProperties, CloudToDeviceMethod
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.servicebus.aio import ServiceBusClient
+from azure.servicebus import ServiceBusMessage
 load_dotenv()
-DEVICE_ID = os.getenv("DEVICE_ID")
+hostname = os.getenv('IOTHUB_HOSTNAME')
+servicebus_connection_str = os.getenv('AZURE_SERVICE_BUS_CONNECTION_STRING')
+queue_name1 = os.getenv('QUEUE_CREATE')
+queue_name2 = os.getenv('QUEUE_CONNECTIONS')
+
+async def create_device(device_id,registry_manager):
+    try:
+        device_connection_string = ""
+        try:
+            device = registry_manager.get_device(device_id)
+            print(f"Production Line {device_id} already exists.")
+        except Exception:
+            device = registry_manager.create_device_with_sas(
+                device_id,
+                primary_key=None,
+                secondary_key=None,
+                status="enabled"
+            )
+            primary_key = device.authentication.symmetric_key.primary_key
+            device_connection_string = f"HostName={hostname};DeviceId={device_id};SharedAccessKey={primary_key}"
+            print(f"Device {device_id} created successfully.")
+            return device, device_connection_string
+    except Exception as e:
+        print(f"Error creating device {device_id}: {e}")
+        return None, None
+
+async def create_devices(device_ids,registry_manager):
+    tasks = [create_device(device_id, registry_manager) for device_id in device_ids]
+    # Gather results
+    results = await asyncio.gather(*tasks)
+    created_devices, device_connection_strings = zip(*results)  
+    return created_devices, device_connection_strings
+
+async def send_device_connection_keys(device_connection_strs):
+    async with ServiceBusClient.from_connection_string(servicebus_connection_str) as client:
+            sender = client.get_queue_sender(queue_name2)
+            async with sender:
+                print("!@")
+                for conn_strs in device_connection_strs:
+                    message = ServiceBusMessage(conn_strs)  
+                    await sender.send_messages(message)
+                    print(f"Sent device CONNECTION STRING")
+
+async def receive_device_ids(registry_manager):
+    created_devices = []
+    created_connection_strs = []
+    async with ServiceBusClient.from_connection_string(servicebus_connection_str) as client:
+        receiver = client.get_queue_receiver(queue_name1)
+        async with receiver:
+            while True:
+                try:
+                    received_msgs = await receiver.receive_messages(max_message_count=100, max_wait_time=1)
+                    if not received_msgs:
+                        print("Likely no more messages there.")
+                        break
+                    for msg in received_msgs:
+                        device_id = str(msg)
+                        devices, connection_strs = await create_devices([device_id], registry_manager)
+                        print(f"Created devices: {devices}")
+                        print(f"Created connection strings: {connection_strs}")
+
+                        created_devices.extend(devices)
+                        created_connection_strs.extend(connection_strs)
+
+                        await receiver.complete_message(msg)
+                except asyncio.TimeoutError:
+                    print("Timeout reached while waiting for messages.")
+                    break
+    print(f"Processed all IDs: {created_connection_strs}")
+    return created_devices, created_connection_strs
+
 async def receive_twin_reported(manager_client, device_id):
     twin = manager_client.get_twin(device_id)
     rep = twin.properties.reported
@@ -36,9 +107,9 @@ async def clear_desired_twin(manager, device_id):
     twin = manager.update_twin(device_id, twin_patch, twin.etag)
     print("desired prop were just cleaned")
 
-async def run_res_error(registry_manager, device_name):
-    reset_method = CloudToDeviceMethod(method_name="reset_err_status", payload=device_name, response_timeout_in_seconds=14)
-    registry_manager.invoke_device_method(DEVICE_ID, reset_method)
+async def run_res_error(registry_manager, device_id):
+    reset_method = CloudToDeviceMethod(method_name="reset_err_status", payload=None, response_timeout_in_seconds=14)
+    registry_manager.invoke_device_method(device_id, reset_method)
 
 async def read_new_lines_from_blob(container_client, blob_name, last_position):
     try:
@@ -87,36 +158,31 @@ async def get_most_recent_blob(container_client, last_processed_name, last_proce
 
     return None, last_processed_name, last_processed_time, last_read_positions
 
-async def process_production(registry_manager, twin_reported, new_data):
-    twin_reported.pop("$metadata", None)
-    twin_reported.pop("$version", None)
+async def process_production(registry_manager, new_data):
     # Process each JSON line
-    try:
-        unique_data = {}
-        for line in new_data:
-            try:
-                device_name = line['DeviceName']
-                if line['ProductionPercent'] != 'NaN':
-                    unique_data[device_name] = line['ProductionRate'] 
-            except json.JSONDecodeError as json_err:
-                print(f"JSON decoding error: {str(json_err)} in line: {line}")
-            except KeyError as key_err:
-                print(f"Key error: {str(key_err)} in line: {key_err}")
+    unique_data = {}
+    for line in new_data:
+        try:
+            device_name = line['DeviceName']
+            if line['ProductionPercent'] != 'NaN':
+                if (line['ProductionRate'] - 10) < 0:
+                    unique_data[device_name] = 0
+                else:
+                    unique_data[device_name] = line['ProductionRate'] - 10
 
-            for device_name, rate in unique_data.items():
-                print(f"{device_name} has {rate}-10")
-                prod_rate = rate - 10
-                formatted_key = device_name.replace(" ", "")
-                if prod_rate < 0:
-                    prod_rate = 0
-                unique_data[device_name] = prod_rate
-                try:
-                    twin_reported[formatted_key] = {"ProductionRate": prod_rate}
-                    twin = registry_manager.get_twin(DEVICE_ID)
-                    twin_patch = Twin(properties=TwinProperties(desired=twin_reported))
-                    twin = registry_manager.update_twin(DEVICE_ID, twin_patch, twin.etag)
-                except Exception as patch:
-                    print(f"twin patch problem: {str(patch)} in line: {line}")
+        except json.JSONDecodeError as json_err:
+            print(f"JSON decoding error: {str(json_err)} in line: {line}")
+        except KeyError as key_err:
+            print(f"Key error: {str(key_err)} in line: {key_err}")
+    try:
+        for device_name, rate in unique_data.items():
+            try:
+                twin_reported = {"ProductionRate": rate}
+                twin = registry_manager.get_twin(device_name)
+                twin_patch = Twin(properties=TwinProperties(desired=twin_reported))
+                twin = registry_manager.update_twin(device_name, twin_patch, twin.etag)
+            except Exception as patch:
+                print(f"twin patch problem: {str(patch)} in line: {line}")
     except KeyError as key_err:
         print(f"Key error 2: {str(key_err)} in line: {line}")
 
@@ -135,9 +201,8 @@ async def process_error_dm(registry_manager, new_data):
                 print(f"Key error: {str(key_err)} in line: {line}")
         for device_name in unique_data:
             try:
-                payload = device_name
-                stop_method = CloudToDeviceMethod(method_name="emergency_stop", payload=payload, response_timeout_in_seconds=14)
-                registry_manager.invoke_device_method(DEVICE_ID, stop_method)
+                stop_method = CloudToDeviceMethod(method_name="emergency_stop", payload=None, response_timeout_in_seconds=14)
+                registry_manager.invoke_device_method(device_name, stop_method)
                 print(f"Success invoking method for device {device_name}")
             except Exception as method_err:
                 print(f"Error invoking method for device {device_name}: {str(method_err)}")
